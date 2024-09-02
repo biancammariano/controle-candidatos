178f5b88cd0f8b62d708ff5d490d4113a3e19775
biancammariano <biancammariano@gmail.com>
2024-09-02 13:27:41 -0300

    app

--- src/App.java
+++
@@ -1,5 +0,0 @@
-public class ParametrosInvalidosException extends Exception {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Processo seletivo");
-    }
-}
